96ec98d4afe31cd8b0dec4d32a4a115938cb53d0
a.vinogradov <a.vinogradov@bpcbt.com>
2023-11-06 14:55:58 +0300

    upd

--- getTsupConfig.js
+++ getTsupConfig.js
@@ -3,12 +3,16 @@ import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extens
 export function getConfig(opts) {
   return {
     entry: opts.entry,
-    format: ['cjs', 'esm'],
+    format: ['esm'],
     target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77'],
     outDir: 'dist',
     dts: true,
     sourcemap: true,
-    // clean: true,
     esbuildPlugins: [esbuildPluginFilePathExtensions({ esmExtension: 'js' })],
+
+    ...(process.env.MODE === 'prod' && {
+      clean: true,
+      format: ['cjs', 'esm'],
+    }),
   };
 }
